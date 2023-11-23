0c807c7696f9c90fd0c0a3d0d5103b20edb1c616
macbookpro <Eng.abdullahyseein@gmail.com>
2023-11-23 20:02:54 +0300

    yeah

--- app/src/main/java/com/example/a11/MainActivity.java
+++ app/src/main/java/com/example/a11/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         spinner.setAdapter(adapter);
 //hello world !!!
-// fm;elfkvdnkdmn,bmd knenm km
+// fm;elfkvdnkdmn,bmd kn
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
